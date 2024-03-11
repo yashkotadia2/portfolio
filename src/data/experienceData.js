485877ae7daf6e9ede743bdd0c470281560f2db9
Yash Kotadia <yashkotadia2@gmail.com>
2024-03-11 12:59:13 +0530

    resume udate

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,14 +1,14 @@
 export const experienceData = [
   {
     id: 1,
-    company: "Forescout Technologies",
+    company: "Laurentian Bank",
     jobtitle: "Reactjs Developer",
     startYear: "2022",
     endYear: "Present",
   },
   {
     id: 2,
-    company: "YoppWorks Inc.",
+    company: "Group Strength Benefits Direct Inc.",
     jobtitle: "Front End Developer",
     startYear: "2020",
     endYear: "2021",
